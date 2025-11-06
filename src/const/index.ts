0398d687012095cd172338ca2dc4cce7b0b5d3d0
yukine <anod100002@gmail.com>
2025-11-06 13:26:47 +0400

    лям так лям

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,4 +1,4 @@
-export const TOTAL_ITEMS: number = 10000
+export const TOTAL_ITEMS: number = 1000000
 export const ITEM_HEIGHT: number = 60
 export const CONTAINER_HEIGHT: number = 500
 export const BUFFER: number = 5
